18b634ab171e2fcce04af3346c44f281cba99391
CaoQuan250 <ggxhffcdgd@gmail.com>
2022-08-28 22:36:33 +0700

    first commit

---
+++ Bank/Bank/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Customer
+    {
+        private string name { get; set; }
+
+        private int age { get; set; }
+
+        private List<BankAccount> accounts { get; set; }
+
+        public Customer(string name, int age, List<BankAccount> accounts)
+        {
+            this.name = name;
+            this.age = age;
+            this.accounts = accounts;
+        }
+    }
+}
---
+++ Bank/Bank/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Bank
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] commands = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(e => e.ToLower())
+                .ToArray();
+
+            Dictionary<int, BankAccount> account = new Dictionary<int, BankAccount>();
+
+            while(commands[0] != "end")
+            {
+                if (commands[0] == "create")
+                {
+                    BankAccount.Create(commands, account);
+                }
+                else if (commands[0] == "deposit")
+                {
+                    BankAccount.Deposit(commands, account);
+                }
+                else if (commands[0] == "withdraw")
+                {
+                    BankAccount.Withdraw(commands, account);
+                }
+                else if (commands[0] == "print")
+                {
+                    BankAccount.Print(commands, account);
+                }
+                commands = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(e => e.ToLower())
+                .ToArray();
+            }
+        }
+    }
+}
